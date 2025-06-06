8767e92e760ba3329fa200bfb9a41b371aa2641f
Nishan2004paudel <neezpau04@gmail.com>
2025-06-06 14:39:39 +0545

    ressolved version errors

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ import pandas as pd
 import pickle
 import hashlib
 from database import add_user, get_user, create_connection
-import streamlit as st
-import sklearn
-st.write("scikit-learn version on server:", sklearn.__version__)
+
+
 
 # Function to hash passwords
 def hash_password(password):
